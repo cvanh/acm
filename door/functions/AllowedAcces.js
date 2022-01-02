8480043a614e9f3e55ad8b6eb8839398dd1a5552
constantijn <constantijn@vanhartesveldt.nl>
2022-01-02 22:21:38 +0000

    work

---
+++ door/functions/AllowedAcces.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const config = require('../config');
+
+async function AllowedAcces(Formatteduid){
+    axios.post(`http://${config.ServerIp}/check`, {
+      "uid": Formatteduid
+    })
+    .then(function (response) {
+      console.log(response.data);
+  
+      if(response.data === true){
+        // open door
+        console.log("opening door")
+        rpio.write(36, rpio.HIGH);
+  
+        rpio.sleep(100);
+        rpio.write(36, rpio.LOW);
+        rpio.msleep(500);
+      }
+  
+  
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  module.exports = AllowedAcces
