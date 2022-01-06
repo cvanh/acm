8e95222d86ef0cf1a0e4dc664eb1fd779c7306b8
constantijn <constantijn@vanhartesveldt.nl>
2022-01-06 19:31:59 +0000

    work

--- door-rs/src/main.rs
+++ door-rs/src/main.rs
@@ -1,7 +1,4 @@
 // stolen from https://github.com/mbr/gpio-rs/blob/master/examples/gpio_inout.rs and https://gitlab.com/jspngh/rfid-rs/-/blob/master/examples/read.rs
-extern crate gpio;
-use gpio::{GpioIn, GpioOut};
-use std::{thread, time};
 use spidev::{Spidev, SpidevOptions, SpiModeFlags};
 use std::io;
 
@@ -9,7 +6,7 @@ extern crate rfid_rs;
 use rfid_rs::picc;
 
 fn create_spi() -> io::Result<Spidev> {
-    let mut spi = Spidev::open("/dev/spidev1.0")?;
+    let mut spi = Spidev::open("/dev/spidev0.0")?;
     let options = SpidevOptions::new()
         .bits_per_word(8)
         .max_speed_hz(20_000)
@@ -19,9 +16,7 @@ fn create_spi() -> io::Result<Spidev> {
     Ok(spi)
 }
 
-
 fn main() {
-    // --------------- rfid reader
     let spi = create_spi().unwrap();
     let mut mfrc522 = rfid_rs::MFRC522 { spi };
     mfrc522.init().expect("Init failed!");
@@ -78,30 +73,5 @@ fn main() {
             mfrc522.halt_a().expect("Could not halt");
             mfrc522.stop_crypto1().expect("Could not stop crypto1");
         }
-    }
-
-
-
-
-
-    // --------------- the door button
-    let mut gpio23 = gpio::sysfs::SysFsGpioInput::open(23).expect("could not open GPIO23");
-    let mut gpio24 = gpio::sysfs::SysFsGpioOutput::open(24).expect("could not open GPIO24");
-
-    // start a thread to toggle the gpio on and off at a different rate
-    let mut value = false;
-    thread::spawn(move || loop {
-        gpio24.set_value(value).expect("could not set gpio24");
-        thread::sleep(time::Duration::from_millis(1000));
-        value = !value;
-    });
-
-    loop {
-        println!(
-            "GPIO23: {:?}",
-            gpio23.read_value().expect("could not read gpio23")
-        );
-
-        thread::sleep(time::Duration::from_millis(100));
     }
 }
